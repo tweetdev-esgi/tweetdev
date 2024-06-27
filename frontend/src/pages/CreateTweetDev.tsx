c4c5c9a5dea080827be358bda7f9b59e87d14c8f
paschyz <paschy2002@gmail.com>
2024-06-27 16:36:45 +0200

    react mdEditor added to create posts

--- frontend/src/pages/CreateTweetDev.tsx
+++ frontend/src/pages/CreateTweetDev.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import MDEditor from "@uiw/react-md-editor";
 
-function CreateTweetDev() {
-    return (
-        <div>
-            Create
-        </div>
-    );
+export default function App() {
+  const [value, setValue] = React.useState("**Hello world!!!**");
+  return (
+    <div className="container m-24">
+      <MDEditor value={value} onChange={setValue} />
+      <MDEditor.Markdown source={value} style={{ whiteSpace: "pre-wrap" }} />
+    </div>
+  );
 }
-
-export default CreateTweetDev;
